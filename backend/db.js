4d06cb68403ebea534e70a9ac0a2bcd777b5da0f
VIPUL070 <sutharvipul007@gmail.com>
2026-04-01 17:21:00 +0530

    Creating Bank related Schema

--- backend/db.js
+++ backend/db.js
@@ -11,8 +11,22 @@ const UserSchema = new mongoose.Schema({
    username: String,
 })
 
+const AccountsSchema = new mongoose.Schema({
+  userId : {
+    type: mongoose.Schema.Types.ObjectId, // Ref to user model , exist user can have balance
+    ref: 'User', //ref from where userID belong
+    required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  }
+})
+
+const Account = mongoose.model('Account' , AccountsSchema)
 const User = mongoose.model('User', UserSchema)
 
 module.exports = {
     User,
+    Account
 }
--- backend/middleware.js
+++ backend/middleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 async function authMiddleware (req,res,next){
   const authHeader = req.headers.authorization
-  if(!authHeader || !authHeader.startsWith('Bearer')){
+  if(!authHeader || !authHeader.startsWith('Bearer ')){
     return res.status(403).json({})
   }
 
